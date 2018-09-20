ae8ebcdba5da0a9f81ccc6da3b28ca482ecc76e2
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-09-20 17:16:56 +0700

    Create list_stack.py

---
+++ minggu-03/praktik/list_stack.py
@@ -0,0 +1,17 @@
+# Berikut ini adalah kodingan dari list_stack.py
+
+>>> stack = [3,4,5]
+>>> stack.append(6)
+>>> stack.append(7)
+
+>>> stack
+#Hasil dari stack 
+# [3, 4, 5, 6, 7]
+
+>>> stack.pop()
+# Hasil dari stack.pop()
+# 7
+
+>>> stack
+# Hasil dari stack setelah di stack.pop()
+# [3, 4, 5, 6]
