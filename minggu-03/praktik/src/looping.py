66eda688de99e39fc2f2f6767e6103e903148f9f
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-09-26 12:26:38 +0700

    Create looping.py

---
+++ minggu-03/praktik/src/looping.py
@@ -0,0 +1,13 @@
+# Contoh looping pada Python
+
+
+>>> knight = {'gallahad': 'the pure','robin':'thebrave'}
+
+# Hampir sama dengan for atau perulangan dibahasa pemograman lain, pertama for dulu apa yang ingin di ulang 
+# kemudian cetak nilai perulangan tersebut
+
+>>> for k, v in knight.items():
+...     print(k,v)
+...
+gallahad the pure
+robin thebrave
