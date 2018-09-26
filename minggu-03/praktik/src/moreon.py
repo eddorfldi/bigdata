b7287e3450e25ee648ff936b07223a3054694691
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-09-26 12:29:46 +0700

    Create moreon.py

---
+++ minggu-03/praktik/src/moreon.py
@@ -0,0 +1,9 @@
+# dibawah ini adalah source code dari contoh more on conditions
+
+# Buat beberapa nilai variable
+>>> string1, string2, string3 = '','Trondheim','Hammer Dance'
+
+# kemudian buat dalam satu variable dan diikuti dengan function or
+>>> not_null = string1 or string2 or string3
+>>> not_null
+'Trondheim'
