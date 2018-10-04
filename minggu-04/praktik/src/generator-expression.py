eb25b8bb71da92455c99639ee744fba1a07ac80c
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 16:41:07 +0700

    Create generator-expression.py

---
+++ minggu-04/praktik/src/generator-expression.py
@@ -0,0 +1,6 @@
+# buat sebuah variable data
+>>> data = 'golf'
+
+# kemudian tampilan data tersebut dalam bentuk list melalui perhitungan range
+>>> list(data[i] for i in range(len(data)-1,-1,-1))
+['f', 'l', 'o', 'g']
