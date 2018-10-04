9f5c8f47a8fd95ed73e950ec2b0e337fd93264b4
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 16:03:59 +0700

    Create handling-exception.py

---
+++ minggu-04/praktik/src/handling-exception.py
@@ -0,0 +1,11 @@
+# Contoh untuk menghandle sebuah exception
+
+>>> def this_fails():
+...     x = 1/0
+...
+>>> try:
+...     this_fails()
+... except ZeroDivisionError as err:
+...     print('Handling run-time error:', err)
+...
+Handling run-time error: division by zero
