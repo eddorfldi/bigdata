5d1e4fee9a882c596d0f5745823c1fdeadfca4da
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:43:42 +0700

    Create old-string.py

---
+++ minggu-04/praktik/src/old-string.py
@@ -0,0 +1,9 @@
+# diatas ini merupakan cara mengimport sebuah fungsi dari math
+
+>>> import math
+
+# setelah di import maka akan coba dengan code dibawah ini
+>>> print('The value of pi is approximately %5.3f.'% math.pi)
+
+# hasil dari print yang di atas
+The value of pi is approximately 3.142.
