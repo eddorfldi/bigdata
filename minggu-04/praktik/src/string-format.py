33c8740b115f0dfaa3ccce57a7c9c568f5ce1573
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:36:23 +0700

    Create string-format.py

---
+++ minggu-04/praktik/src/string-format.py
@@ -0,0 +1,9 @@
+# Contoh diatas merupakan contoh menprint sebuah perintah menggunakan beberapa array didalamnya
+
+>>> print('This {food} is {adjective}.'.format(
+
+# masukkan nilai array yang akan ditampilkan
+...     food='spam', adjective='absolutely horrible'))
+
+# contoh pengaplikasiannya
+This spam is absolutely horrible.
