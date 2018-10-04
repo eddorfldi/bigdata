13ee1d011444508c7ce7484b395f9a7a491ce98e
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:24:44 +0700

    Create dir_function.py

---
+++ minggu-04/praktik/src/dir_function.py
@@ -0,0 +1,9 @@
+# Di atas merupakan sebuah penggunaan dari fungsi dir, dimana akan menampilkan fungsi apa saja yang ada di dir.
+
+>>> a = [1,2,3,4,5]
+>>> fib = fibo.fib
+>>> dir()
+
+# isi dari fungsi dir()
+['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', 
+'__package__', '__spec__', 'a', 'fib', 'fibo', 'sys']
