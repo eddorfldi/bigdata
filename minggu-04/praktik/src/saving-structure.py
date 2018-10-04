3093eb6b58473c03be5f47f55ad78bc61baf7e2d
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:52:39 +0700

    Create saving-structure.py

---
+++ minggu-04/praktik/src/saving-structure.py
@@ -0,0 +1,10 @@
+# Menampilkan data dalam bentuk json, 
+
+# Pertama import fungsi atau module dari json
+>>> import json
+
+# Kemudian coba dumps dari json tersebut, dumps hampir serupa print tetapi khusus json
+>>> json.dumps([1,'simple','list'])
+
+# Hasil keluaran json.dumps diatas
+'[1, "simple", "list"]'
