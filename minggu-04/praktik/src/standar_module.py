bdd3ec8610f1d8cdb747bb2e6e00d6d536b1e1ce
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:21:44 +0700

    Create standar_module.py

---
+++ minggu-04/praktik/src/standar_module.py
@@ -0,0 +1,13 @@
+# Dibawah ini merupakan sebuah cara import untuk mengimport system
+
+>>> import sys
+>>> sys.ps1
+'>>> '
+
+# ini akan membuat direktori atau kemana si sys.ps1 ini harus pergi
+>>> sys.ps1 = 'C>'
+C>print('Yeah!')
+
+#Hasil dari perintah diatas
+Yeah!
+C>
