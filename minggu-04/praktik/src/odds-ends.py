0ad640ff512958b93ee235cf5aa06f669d39742a
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 16:32:25 +0700

    Create odds-ends.py

---
+++ minggu-04/praktik/src/odds-ends.py
@@ -0,0 +1,9 @@
+# contoh membuat sebuah class yang dimana pengisian data tidak terjadi daidalam kelas tetapi diluar class tersebut.
+
+>>> class Employee:
+...     pass
+...
+>>> john = Employee()
+>>> john.name = 'john doe'
+>>> john.dept = 'computer lab'
+>>> john.salary = 1000
