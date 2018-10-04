903b63c7a1e6d426cfd56ae7d44fb9f865ec904c
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 16:26:52 +0700

    Create random-remarks.py

---
+++ minggu-04/praktik/src/random-remarks.py
@@ -0,0 +1,12 @@
+# beriut ini adalah contoh dari random remarks dari python
+
+>>> def f1(self, x, y):
+...     return min(x, x+y)
+...
+>>> class C:
+...     f = f1
+...     def g(self):
+...             return 'hello world'
+...     h = g
+...
+>>>
