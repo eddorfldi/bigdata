c4b3fbbf81d22f52b615e1be63128a5a1c7b627b
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:19:57 +0700

    Create more-module.py

---
+++ minggu-04/praktik/src/more-module.py
@@ -0,0 +1,8 @@
+# Diatas ini merupakan implementasi dari module yang sudah di buat sebelumnya
+
+# Dibawah cara pemanggilan module
+>>> import fibo as fib
+>>> fib.fib(500)
+
+# Berikut ini adalah hasil dari fib.fib(500)
+# 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
