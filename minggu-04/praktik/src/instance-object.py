d9a76aae96acd89a4d03e90c88e99657ab919ef8
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 16:22:57 +0700

    Create instance-object.py

---
+++ minggu-04/praktik/src/instance-object.py
@@ -0,0 +1,9 @@
+# disini bertujuan untuk membuat, mengunakan while dan menghapus dari sebuah variable
+
+>>> x.counter = 1
+>>> while x.counter < 10:
+...     x.counter = x.counter * 2
+...
+>>> print(x.counter)
+16
+>>> del x.counter
