cc140780462b952a993681fa8cce8618e30e7705
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 15:27:44 +0700

    Create fancier_output.py

---
+++ minggu-04/praktik/src/fancier_output.py
@@ -0,0 +1,10 @@
+# Dibawah ini adalah fungsi output dari fancier, dimana pemanggilan data tak perlu menggunakan print
+
+>>> year = 2016
+>>> event = 'Referendum'
+
+# f sebagai pengganti print, dan didalamnya bisa terdapat array yang akan ditampilkan
+>>> f'Results of the {year} {event}'
+
+# Hasil dari diatas
+'Results of the 2016 Referendum'
