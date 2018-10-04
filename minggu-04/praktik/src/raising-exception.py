4e87ac059b2bc0f5edaad2e07a2475bc25f88f98
Muhammad Redho Isra Rifaldi <reirifaldi@gmail.com>
2018-10-04 16:06:46 +0700

    Create raising-exception.py

---
+++ minggu-04/praktik/src/raising-exception.py
@@ -0,0 +1,12 @@
+# berikut ini adalah cara menampilkan bagian mana yang error dan membuat nama dari error tersebut.
+
+>>> try:
+...     raise NameError('HiThere')
+... except NameError:
+...     print('An exception flew by!')
+...     raise
+...
+An exception flew by!
+Traceback (most recent call last):
+  File "<stdin>", line 2, in <module>
+NameError: HiThere
